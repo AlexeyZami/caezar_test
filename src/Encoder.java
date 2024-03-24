b2b52cd16fceadac06cd8ea0a3e5a2c0c60719d5
Alexey Lyalyakin <varan560045@yandex.ru>
2024-03-24 17:53:13 +0300

    created Encoder.java

---
+++ src/Encoder.java
@@ -0,0 +1,14 @@
+import java.io.FileNotFoundException;
+import java.util.List;
+
+public class Encoder {
+    public static List<Integer> inputList;
+
+    static {
+        try {
+            inputList = convertToList(UserInteraction.getPath());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
